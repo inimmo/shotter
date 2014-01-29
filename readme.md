9a38379cba32ddc9fdfff82ff5102a6ead3c0add
inimmo <inimmo@gmail.com>
2014-01-29 16:25:08 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+Shotter
+=======
+
+Shotter is a script for PhantomJS that allows you to specify an array of CSS
+selectors, and capture screenshots of each one. It is very much in its
+infancy.
---
+++ shotter.phantom.js
@@ -0,0 +1,55 @@
+var page = require('webpage').create(),
+	system = require('system'),
+	fs = require('fs'),
+	config_filename = system.args[1],
+	config = null
+;
+
+if (!config_filename)
+{
+	console.log("Usage: phantomjs shotter.js <config_file>");
+	phantom.exit(1);
+}
+
+var config = JSON.parse(fs.read(config_filename));
+
+if (!fs.exists(config.output_dir))
+{
+	fs.makeDirectory(config.output_dir);
+}
+
+page.onConsoleMessage = function(msg, line, source) { console.log(msg); }
+
+page.open(config.target_url, function (status) {
+	if (status !== 'success')
+	{
+		phantom.exit(1);
+	}
+	
+	setTimeout(function () {
+		var shots = page.evaluate(function (selectors) {
+			var ret = [],
+				elem;
+			
+			for (var i = 0; i < selectors.length; i++)
+			{
+				elem = document.querySelectorAll(selectors[i].css_selector)[0];
+				
+				if (elem)
+				{
+					selectors[i].clip_rect = elem.getBoundingClientRect();
+				}
+			}
+			
+			return selectors;
+		}, config.selectors);
+		
+		for (var i = 0; i < shots.length; i++)
+		{
+			page.clipRect = shots[i].clip_rect;
+			page.render(config.output_dir + '/' + shots[i].descriptor + '.png');
+		}
+		
+		phantom.exit(0);
+	}, 200);
+});
